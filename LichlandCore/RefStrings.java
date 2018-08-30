93d78c5b1f07389c98f00a76302a996d15e099ac
GargatheOro <42824479+GargatheOro@users.noreply.github.com>
2018-08-29 20:23:40 -0600

    Update RefStrings.java

--- LichlandCore/RefStrings.java
+++ LichlandCore/RefStrings.java
@@ -4,8 +4,8 @@ public class RefStrings {
 	public static final String MODID = "lichland";
 	public static final String NAME = "Lichland Mod";
 	public static final String VERSION = "0.4.3";
-	public static final String CLIENTSIDE = "com.GargatheOro.LichlandCore.ClientProxy";
-	public static final String SERVERSIDE = "com.GargatheOro.LichlandCore.ServerProxy";
+	public static final String CLIENTSIDE = "com.LichlandDevs.LichlandCore.ClientProxy";
+	public static final String SERVERSIDE = "com.LichlandDevs.LichlandCore.ServerProxy";
 	public static final String NETWORK_CHANNEL_NAME = "Network";
-	public static final String GUI_FACTORY_CLASS = "com.GargatheOro.LichlandCore.GUIFactory";
+	public static final String GUI_FACTORY_CLASS = "com.LichlandDevs.LichlandCore.GUIFactory";
 }
